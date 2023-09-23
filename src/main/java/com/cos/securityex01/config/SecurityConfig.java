a9aded3a81825c7598d3db116259a513350a4426
yso8296 <yso829612@naver.com>
2023-09-23 22:09:06 +0900

    session을 이용한 회원가입/로그인 구현

---
+++ src/main/java/com/cos/securityex01/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cos.securityex01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 해당 메서드의 리턴되는 오브젝트를 ioc로 등록해준다
+    @Bean
+    public BCryptPasswordEncoder encoderPwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") // /login 주소가 호출 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌
+                .defaultSuccessUrl("/");
+
+    }
+}
---
+++ src/main/java/com/cos/securityex01/controller/IndexController.java
@@ -0,0 +1,76 @@
+package com.cos.securityex01.controller;
+
+import com.cos.securityex01.model.User;
+import com.cos.securityex01.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class IndexController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@GetMapping({ "", "/" })
+	public @ResponseBody String index() {
+		return "인덱스 페이지입니다.";
+	}
+
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "유저 페이지입니다.";
+	}
+
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "어드민 페이지입니다.";
+	}
+
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "매니저 페이지입니다.";
+	}
+
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
+	}
+
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
+	}
+
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user); // 패스워드 암호화가 안되서 시큐리티로 로그인 불가
+
+		return "redirect:/loginForm";
+	}
+
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+	@GetMapping("/info")
+	public @ResponseBody String data() {
+		return "데이터정보";
+	}
+}
